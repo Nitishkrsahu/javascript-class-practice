8bd70692ab211a6154f661cf0bdb24b9efd91cbb
Nitish <nitishkrsahu123@gmail.com>
2025-04-21 16:47:35 +0530

    some javascript code practice is added

---
+++ src/index.js
@@ -0,0 +1,216 @@
+"use strict";
+
+// var a = 10;
+// console.log(a);
+
+// let b = 20;
+// console.log(b);
+
+// const c = 30;
+// console.log(c);
+
+// var x = 1;
+// if (true) {
+//   var x = 2;
+//   console.log(x);
+// }
+// console.log(x);
+
+// let y = 1;
+// if (true) {
+//   let y = 2;
+//   console.log(y);
+// }
+// console.log(y);
+
+// const z = 1;
+// if (true) {
+//   const z = 2;
+//   console.log(z);
+// }
+// console.log(z);
+
+// var g = 10;
+// var g = 20;
+// console.log(g);
+
+// let h = 10;
+// let h = 20;
+// console.log(h);
+
+// const i = 10;
+// const i = 20;
+// console.log(i);
+
+// var j = 10;
+// let j = 20;
+// console.log(j);
+
+// let k = 10;
+// var k = 20;
+// console.log(k);
+
+// const l = 10;
+// var l = 20;
+// console.log(l);
+
+// let m = 5;
+// m = 10;
+// console.log(m);
+
+// const n = 5;
+// n = 10;
+// console.log(n);
+
+// const arr = [1, 2, 3];
+// arr.push(4);
+// console.log(arr);
+
+// const arr = [1, 2, 3];
+// arr = [4, 5, 6];
+// console.log(arr);
+
+// let num = 5;
+// {
+//   let num = 10;
+//   console.log(num);
+// }
+// console.log(num);
+
+// var num = 5;
+// {
+//   var num = 10;
+//   console.log(num);
+// }
+// console.log(num);
+
+// const num = 5;
+// {
+//   const num = 10;
+//   console.log(num);
+// }
+// console.log(num);
+
+// let a = 1;
+// a = "hello";
+// console.log(a);
+
+// const b = 1;
+// b = "hello";
+// console.log(b);
+
+// function test() {
+//     if (true) {
+//       var c = 10;
+//     }
+//     console.log(c);
+//   }
+//   test();
+
+// function test() {
+//     if (true) {
+//       let d = 10;
+//     }
+//     console.log(d);
+//   }
+//   test();
+
+// function test() {
+//     if (true) {
+//       const e = 10;
+//     }
+//     console.log(e);
+//   }
+//   test();
+
+// const PI = 3.14;
+// PI = 3.14159;
+// console.log(PI);
+
+// let x;
+// console.log(x);
+// x = 5;
+// console.log(x);
+
+// const y;
+// console.log(y);
+// y = 5;
+// console.log(y);
+
+// let a = 1;
+// {
+//   let a = 2;
+//   console.log(a);
+// }
+// console.log(a);
+
+// var b = 1;
+// {
+//   let b = 2;
+//   console.log(b);
+// }
+// console.log(b);
+
+// const d = 1;
+// {
+//   var d = 2;
+//   console.log(d);
+// }
+// console.log(d);
+
+// const arr = [1, 2, 3];
+// arr[0] = 10;
+// console.log(arr);
+
+// let x = 10;
+// function foo() {
+//   console.log(x);
+//   let x = 20;
+// }
+// foo();
+
+// let y = 10;
+// function bar() {
+//   console.log(y);
+//   var y = 20;
+// }
+// bar();
+
+// var z = 10;
+// function baz() {
+//   console.log(z);
+//   var z = 20;
+// }
+// baz();
+
+// const a = 10;
+// function test() {
+//   const a = 20;
+//   console.log(a);
+// }
+// test();
+// console.log(a);
+
+// var b = 10;
+// function test() {
+//   let b = 20;
+//   console.log(b);
+// }
+// test();
+// console.log(b);
+
+// let c = 10;
+// function test() {
+//   var c = 20;
+//   console.log(c);
+// }
+// test();
+// console.log(c);
+
+// const d = 10;
+// function test() {
+//   const d = 20;
+//   console.log(d);
+// }
+// test();
+// console.log(d);
